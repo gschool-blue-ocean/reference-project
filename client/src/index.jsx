98bddb4b5d84da2cd10166f76c81d60804fa2a74
Danny <lunde.andrews@gmail.com>
2023-02-28 20:05:46 -0500

    Final npm fixes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App/App";
-import isOdd from "is-odd";
-
-console.log(isOdd(5));
 
 createRoot(document.getElementById("root")).render(<App />);
